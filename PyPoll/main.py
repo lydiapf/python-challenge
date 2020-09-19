8e1c58f8c012c1da6f0a24e59fcb65dbb8ceb46e
lydiapf <lydia291@gmail.com>
2020-09-19 10:40:31 -0400

    test push

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,17 +6,18 @@ election_data = os.path.join('Resources', 'budget_data.csv')
 total_votes = len(election_data)
 total_votes
 
-candidate_count = election_data["Candidate"].value_counts()
-
-import os
-import csv
-
-election_data = os.path.join('Resources', 'budget_data.csv')
-
 total_votes = 0
 candidates = []
 votes = []
-percent_votes []
+percent_votes = []
+
+with open(election_data, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter= ',')
+    
+    header = next(reader)
+    
+    print(header)
+
 
 
 total_votes = len(election_data)
