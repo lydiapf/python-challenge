55ef491211c7fdfc886cad3d532810901276480d
lydiapf <lydia291@gmail.com>
2020-09-19 10:18:37 -0400

    PyPoll lists

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,19 @@ total_votes = len(election_data)
 total_votes
 
 candidate_count = election_data["Candidate"].value_counts()
+
+import os
+import csv
+
+election_data = os.path.join('Resources', 'budget_data.csv')
+
+total_votes = 0
+candidates = []
+votes = []
+percent_votes []
+
+
+total_votes = len(election_data)
+total_votes = 
+
+candidate_count = election_data["Candidate"].value_counts()
