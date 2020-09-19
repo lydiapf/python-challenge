8cc91038da68f963a1e79a9abef8cd50861eccb1
lydiapf <lydia291@gmail.com>
2020-09-19 11:07:49 -0400

    PyPoll_rows

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,32 @@
 import os
 import csv
 
-election_data = os.path.join('Resources', 'budget_data.csv')
-
-total_votes = len(election_data)
-total_votes
+election_data = os.path.join('Resources', 'election_data.csv')
 
 total_votes = 0
 candidates = []
 votes = []
 percent_votes = []
 
-with open(election_data, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter= ',')
+with open(election_data, 'r') as elections:
+    reader = csv.reader(elections, delimiter= ',')
     
     header = next(reader)
-    
     print(header)
+
+    for row in elections:
+        total_votes += 1
+
+        if row[2]not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            votes[index] += 1
 
 
 
 total_votes = len(election_data)
-total_votes = 
 
-candidate_count = election_data["Candidate"].value_counts()
+candidates = election_data["Candidate"].value_counts()
