b1b5ef2bee35cdd7046060521c8be484655db426
lydiapf <lydia291@gmail.com>
2020-09-19 15:09:49 -0400

    list comprehension

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,3 +6,19 @@ election_data = os.path.join('Resources', 'election_data.csv')
 candidate = []
 votes = []
 unique_name = []
+
+with open(election_data, 'r') as csvfile:
+
+    reader = csv.reader(csvfile, delimiter='r')
+
+    header = next(reader)
+
+    for row in reader:
+        candidate.append(row[2])
+
+    count_candidates = [[x,candidate.count(x)] for x in set(candidate)]
+
+    for row in count_candidates:
+        unique_name.append(row[0])
+        votes.append(row[1])
+
